0ec3166f415f0f2e85aa89dde3bea020bdbefa88
Ranjeet Kashid <folec31488@aaorsi.com>
2023-06-28 07:07:50 +0000

    updated models and dbt mini project

--- models/int/fact_cohort_analysis.sql
+++ models/int/fact_cohort_analysis.sql
@@ -1,14 +1,19 @@
 with fact_cohort as (
 select
-customer_id,
-min(order_date) as first_order_date,
-max(order_date) as last_order_date,
-count (distinct order_id) as total_orders,
-sum(amount) as total_revenue,
-round((max(order_date) - min(order_date))/count (distinct order_id)) as average_days_between_orders,
-CURRENT_TIMESTAMP as CreatedAt
-from {{ref("fct_orders")}}
-group by customer_id
-order by customer_id asc)
+    -- rollups
+    a.customer_id,
+    -- aggregates 
+    min(case when a.order_status = 'completed' and b.status = 'success' then a.order_date end) as first_order_date,
+    max(a.order_date) as last_order_date,
+    count (distinct a.order_id) as total_orders,
+    sum(a.amount) as total_revenue,
+    round((max(a.order_date) - min(a.order_date)) / 
+        count (distinct a.order_id)) as average_days_between_orders,
+    CURRENT_TIMESTAMP as CreatedAt
+from {{ref("fct_orders")}} as a
+join {{ref("stg_payments")}} as b
+on a.order_id = b.order_id
+group by 1
+order by 1 asc)
 
 select * from fact_cohort
